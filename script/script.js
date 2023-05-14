2d7165740a7d0611e056ed104edee341de2b3a5f
Jim2510 <guglielmino2510@gmail.com>
2023-05-14 20:38:05 +0200

    Aggiunta pagina CV da completare con relativo codice css, rimozione emailjs

--- script/script.js
+++ script/script.js
@@ -9,16 +9,16 @@ toggleBtn.onclick = function () {
   toggleBtnIcon.classList = isOpen
   ? 'fa-solid fa-xmark'
   : 'fa-solid fa-bars'
-}
+};
 
-function sendMail() {
-  var params = {
-    from_name : document.getElementbyId('fullName').value,
-    email_id : document.getElementById('email_id').value,
-    message : document.getElementById('message').value,
-    phone_number : document.getElementById('phone_number').value
-  }
-  emailjs.send('service_5r2ot1w', 'template_urfoice', params).then(function (res){
-    alert('success!' + res.status)
-  })
-}
+// function sendMail() {
+//   var params = {
+//     from_name : document.getElementById('fullName').value,
+//     email_id : document.getElementById('email_id').value,
+//     message : document.getElementById('message').value,
+//     phone_number : document.getElementById('phone_number').value
+//   }
+//   emailjs.sendForm('service_5r2ot1w', 'template_urfoice', params).then(function (res){
+//     alert('success!' + res.status)
+//   })
+// }
