d308cc49a4b805ec96e39990961757a1a477c9c6
Jim2510 <guglielmino2510@gmail.com>
2023-06-21 11:18:35 +0200

    Modifica stile home section, modifica logo

--- src/dist/main.dev.js
+++ src/dist/main.dev.js
@@ -6,78 +6,80 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var toggleBtn = document.querySelector('.toggle_btn');
-var toggleBtnIcon = document.querySelector('.toggle_btn i');
-var dropDownMenu = document.querySelector('.dropdown_menu');
-var revealOptions = {
-  delay: 200,
-  distance: '50px',
-  origin: 'bottom',
-  easing: 'ease-out',
-  interval: 200
-};
-var aboutText = document.querySelector('.about-text');
-var readMoreButtons = document.querySelectorAll('.read');
-var overlay = document.getElementById('overlay');
-var closeBtn = document.querySelector('.close-btn');
-var isAboutTextShown = false;
-var isOverlayOpen = false;
-toggleBtn.addEventListener('click', function () {
-  dropDownMenu.classList.toggle('open');
-  toggleBtnIcon.classList.toggle('fa-xmark');
-  toggleBtnIcon.classList.toggle('fa-bars');
-});
-ScrollReveal().reveal('.box', revealOptions);
-ScrollReveal().reveal('.home-text', _objectSpread({}, revealOptions, {
-  origin: 'left',
-  duration: 2000
-}));
-ScrollReveal().reveal('.about-text', _objectSpread({}, revealOptions, {
-  origin: 'right',
-  duration: 2000,
-  beforeReveal: function beforeReveal(el) {
-    el.classList.add('show');
-  }
-}));
-window.addEventListener('scroll', function () {
-  if (isElementInViewport(aboutText) && !isAboutTextShown) {
-    aboutText.classList.add('show');
-    isAboutTextShown = true;
-  }
-});
-ScrollReveal().reveal('.main-text', _objectSpread({}, revealOptions, {
-  origin: 'bottom',
-  duration: 1000,
-  opacity: 0
-}));
-ScrollReveal().reveal('.row', _objectSpread({}, revealOptions, {
-  origin: 'bottom',
-  duration: 1000,
-  opacity: 0,
-  interval: 200
-}));
-readMoreButtons.forEach(function (button) {
-  button.addEventListener('click', function () {
-    overlay.style.display = 'flex';
-    disableScroll();
-    isOverlayOpen = true;
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleBtn = document.querySelector('.toggle_btn');
+  var toggleBtnIcon = document.querySelector('.toggle_btn i');
+  var dropDownMenu = document.querySelector('.dropdown_menu');
+  var revealOptions = {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    interval: 200
+  };
+  var aboutText = document.querySelector('.about-text');
+  var readMoreButtons = document.querySelectorAll('.read');
+  var overlay = document.getElementById('overlay');
+  var closeBtn = document.querySelector('.close-btn');
+  var isAboutTextShown = false;
+  var isOverlayOpen = false;
+  var sections = document.querySelectorAll('section'); // Rimuovere la classe "active" da tutti i link
+
+  var navLinks = document.querySelectorAll('.links a');
+  navLinks.forEach(function (link) {
+    link.classList.remove('active');
   });
+  toggleBtn.addEventListener('click', function () {
+    dropDownMenu.classList.toggle('open');
+    toggleBtnIcon.classList.toggle('fa-xmark');
+    toggleBtnIcon.classList.toggle('fa-bars');
+  });
+  ScrollReveal().reveal('.box', revealOptions);
+  ScrollReveal().reveal('.home-text', _objectSpread({}, revealOptions, {
+    origin: 'left',
+    duration: 2000
+  }));
+  ScrollReveal().reveal('.about-text', _objectSpread({}, revealOptions, {
+    origin: 'right',
+    duration: 2000,
+    beforeReveal: function beforeReveal(el) {
+      el.classList.add('show');
+    }
+  }));
+  ScrollReveal().reveal('.main-text', _objectSpread({}, revealOptions, {
+    origin: 'bottom',
+    duration: 1000,
+    opacity: 0
+  }));
+  ScrollReveal().reveal('.row', _objectSpread({}, revealOptions, {
+    origin: 'bottom',
+    duration: 1000,
+    opacity: 0,
+    interval: 200
+  }));
+  readMoreButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      overlay.style.display = 'flex';
+      disableScroll();
+      isOverlayOpen = true;
+    });
+  });
+  closeBtn.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    enableScroll();
+    isOverlayOpen = false;
+  });
+
+  function isElementInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  }
+
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  function enableScroll() {
+    document.body.style.overflow = 'auto';
+  }
 });
-closeBtn.addEventListener('click', function () {
-  overlay.style.display = 'none';
-  enableScroll();
-  isOverlayOpen = false;
-});
-
-function isElementInViewport(element) {
-  var rect = element.getBoundingClientRect();
-  return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-}
-
-function disableScroll() {
-  document.body.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-  document.body.style.overflow = 'auto';
-}
--- src/main.js
+++ src/main.js
@@ -1,93 +1,95 @@
-const toggleBtn = document.querySelector('.toggle_btn');
-const toggleBtnIcon = document.querySelector('.toggle_btn i');
-const dropDownMenu = document.querySelector('.dropdown_menu');
-const revealOptions = {
-  delay: 200,
-  distance: '50px',
-  origin: 'bottom',
-  easing: 'ease-out',
-  interval: 200,
-};
-const aboutText = document.querySelector('.about-text');
-const readMoreButtons = document.querySelectorAll('.read');
-const overlay = document.getElementById('overlay');
-const closeBtn = document.querySelector('.close-btn');
-let isAboutTextShown = false;
-let isOverlayOpen = false;
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.querySelector('.toggle_btn');
+  const toggleBtnIcon = document.querySelector('.toggle_btn i');
+  const dropDownMenu = document.querySelector('.dropdown_menu');
+  const revealOptions = {
+    delay: 200,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-out',
+    interval: 200,
+  };
+  const aboutText = document.querySelector('.about-text');
+  const readMoreButtons = document.querySelectorAll('.read');
+  const overlay = document.getElementById('overlay');
+  const closeBtn = document.querySelector('.close-btn');
+  let isAboutTextShown = false;
+  let isOverlayOpen = false;
+  const sections = document.querySelectorAll('section');
 
-toggleBtn.addEventListener('click', () => {
-  dropDownMenu.classList.toggle('open');
-  toggleBtnIcon.classList.toggle('fa-xmark');
-  toggleBtnIcon.classList.toggle('fa-bars');
-});
+  // Rimuovere la classe "active" da tutti i link
+  const navLinks = document.querySelectorAll('.links a');
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+  });
 
-ScrollReveal().reveal('.box', revealOptions);
+  toggleBtn.addEventListener('click', () => {
+    dropDownMenu.classList.toggle('open');
+    toggleBtnIcon.classList.toggle('fa-xmark');
+    toggleBtnIcon.classList.toggle('fa-bars');
+  });
 
-ScrollReveal().reveal('.home-text', {
-  ...revealOptions,
-  origin: 'left',
-  duration: 2000,
-});
+  ScrollReveal().reveal('.box', revealOptions);
 
-ScrollReveal().reveal('.about-text', {
-  ...revealOptions,
-  origin: 'right',
-  duration: 2000,
-  beforeReveal: (el) => {
-    el.classList.add('show');
-  },
-});
+  ScrollReveal().reveal('.home-text', {
+    ...revealOptions,
+    origin: 'left',
+    duration: 2000,
+  });
 
-window.addEventListener('scroll', () => {
-  if (isElementInViewport(aboutText) && !isAboutTextShown) {
-    aboutText.classList.add('show');
-    isAboutTextShown = true;
+  ScrollReveal().reveal('.about-text', {
+    ...revealOptions,
+    origin: 'right',
+    duration: 2000,
+    beforeReveal: (el) => {
+      el.classList.add('show');
+    },
+  });
+
+  ScrollReveal().reveal('.main-text', {
+    ...revealOptions,
+    origin: 'bottom',
+    duration: 1000,
+    opacity: 0,
+  });
+
+  ScrollReveal().reveal('.row', {
+    ...revealOptions,
+    origin: 'bottom',
+    duration: 1000,
+    opacity: 0,
+    interval: 200,
+  });
+
+  readMoreButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      overlay.style.display = 'flex';
+      disableScroll();
+      isOverlayOpen = true;
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    overlay.style.display = 'none';
+    enableScroll();
+    isOverlayOpen = false;
+  });
+
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function disableScroll() {
+    document.body.style.overflow = 'hidden';
+  }
+
+  function enableScroll() {
+    document.body.style.overflow = 'auto';
   }
 });
-
-ScrollReveal().reveal('.main-text', {
-  ...revealOptions,
-  origin: 'bottom',
-  duration: 1000,
-  opacity: 0,
-});
-
-ScrollReveal().reveal('.row', {
-  ...revealOptions,
-  origin: 'bottom',
-  duration: 1000,
-  opacity: 0,
-  interval: 200,
-});
-
-readMoreButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    overlay.style.display = 'flex';
-    disableScroll();
-    isOverlayOpen = true;
-  });
-});
-
-closeBtn.addEventListener('click', () => {
-  overlay.style.display = 'none';
-  enableScroll();
-  isOverlayOpen = false;
-});
-
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-function disableScroll() {
-  document.body.style.overflow = 'hidden';
-}
-
-function enableScroll() {
-  document.body.style.overflow = 'auto';
-}
